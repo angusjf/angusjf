a0dd2656d18cbadf65710b936b45cc06a191642a
Angus <angus.findlay.mb@gmail.com>
2016-10-09 09:08:52 +0100

    60fps animation, side collision, click me message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var ctx, c;
-var running = false;
 var objects = [];
+var grav = 0.45;
+var bounceY = 0.95, bounceX = 0.9;
+var fadeAmount = 0.05;
 
 function Ball(x, y, vx, vy, r, a) { //like a class for ball objects
 	this.x = x;
@@ -11,36 +13,50 @@ function Ball(x, y, vx, vy, r, a) { //like a class for ball objects
 	this.a = a;
 }
 
-window.onload = function () { //called when page is loaded
+function start() {
 	c = document.getElementById("magic-canvas");
 	c.width = 170;
 	c.height = 170;
+	c.onselectstart = function () { return false; }
 	ctx = c.getContext("2d");
 	ctx.font = "20px Menlo";
-	ctx.fillStyle = 'rgba(0,0,0, 0.1)'; //transparency
-	ctx.fillText("click me", 34, c.height / 2 + 5);
-	setInterval(update, 33); //call update at 30fps (every 33ms)
+	window.requestAnimationFrame(update);
 }
 
 function clicked() { //called on click in html
-	if (!running) running = true;
-	objects.push(new Ball((Math.random() * (c.width - 16)) + 8, 16, Math.random() * 6 - 3, 0, 8, 0.5));
+	objects.push(
+		new Ball(
+			c.width/2, 8, //pos
+			Math.random() * 6 - 3, 0, //vel
+			Math.random() * 4 + 8, 0.5 //size / alpha
+		)
+	);
 }
 
-function update() { if (running) {
+function update() {
 	objects.forEach(function(i) { //cycle through every object
-		i.vy += 2.5; //gravity
-		i.x += i.vx;
+		i.vy += grav; //gravity
+		i.x += i.vx; //add velocity
 		i.y += i.vy;
-		if (i.y + i.r > c.height) { //collisions
+
+		//collisions
+		if (i.y + i.r > c.height) { //ground
 			i.y = c.height - i.r; 
-			i.vy *= -0.9;
+			i.vy *= -bounceY;
+			i.a -= fadeAmount; //fade on bounce
+			if (i.a <= 0) objects.shift(); //if faded, remove it
 		}
-		i.a -= 0.0045;
-		if (i.a <= 0) objects.shift(); //if faded, remove it
+
+		if (i.x - i.r < 0) { //left
+			i.x = i.r;
+			i.vx *= -bounceX;
+		} else if (i.x + i.r > c.width) { //right
+			i.x = c.width - i.r;
+			i.vx *= -bounceX;
+		}
 	});
 	draw();
-}}
+}
 
 function draw() {
 	ctx.clearRect(0, 0, c.width, c.height); //clear screen
@@ -50,4 +66,12 @@ function draw() {
 		ctx.arc(i.x, i.y, i.r, 0, 2 * Math.PI);
 		ctx.fill();
 	});
+	if (objects.length == 0) { //if no objects show message
+		ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+		ctx.fillText("click me", 34, c.height / 2 + 5);
+	}
+	window.requestAnimationFrame(update);
 }
+
+//start called when page is loaded
+window.onload = start;
