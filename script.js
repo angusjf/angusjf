a7161bd19fdd1d2469df91e540fcbd0b6cc528d3
Angus Macbook <angus.findlay.mb@gmail.com>
2017-05-21 13:33:36 +0100

    removed content, added new animatin

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 var canvasName = "magic-canvas";
 var ctx, c;
 var paused = true;
-var objects = [];
-var grav = 0.03;
-var startFade = 0.1, fadeAmount = 0.00015;
-var minSize = 4, maxSize = 8;
 var closeButton;
 
-/*
- * like a class for ball objects
- */
-function Ball(x, y, vx, vy, r) {
-	this.x = x;
-	this.y = y;
-	this.vx = vx;
-	this.vy = vy;
-	this.ax = 0;
-	this.ay = grav;
-	this.r = r;
-}
-
 function start() {
-	closeButton = document.getElementById("close-button");
+	closeButton = document.createElement("button");
+	closeButton.classList.add('link');
+	closeButton.id = 'close-button';
 	closeButton.onclick = togglePaused;
+	closeButton.innerHTML = '&#9654;';
+	document.getElementById("container").appendChild(closeButton);
 	c = document.getElementById(canvasName);
 	c.width = document.body.clientWidth;
 	c.height = document.body.clientHeight;
@@ -30,78 +17,12 @@ function start() {
 	ctx = c.getContext("2d");
 	ctx.font = "20px Menlo";
 	window.requestAnimationFrame(update);
-}
-
-function update() {
-	if (!paused) {
-		spawnBall();
-		objects.forEach(updateObject);
-		draw();
-	}
-	window.requestAnimationFrame(update);
-}
-
-function updateObject(i) {
-
-	/*
-	 * Acceleration
-	 */
-	i.vx += i.ax;
-	i.vy += i.ay;
-
-	/*
-	 * Collisions - ground
-	if (i.y + i.r + i.vy > c.height) {
-		i.vx *= -0.1; i.vy *= -0.1;
-		//i.ax = 0; i.ay = 0;
-	} 
-	 */
-
-	/*
-	 * Collisions - other balls
-	objects.forEach(function(j) { //cycle through every object
-		if (j != i) { //other balls
-			var dx = (i.x + i.vx + i.r) - (j.x + j.vx + j.r);
-			var dy = (i.y + i.vy + i.r) - (j.y + j.vy + j.r);
-			var distance = Math.sqrt(dx * dx + dy * dy);
-			if (distance < i.r + j.r) {
-				i.vx *= -0.2; i.vy *= -0.2;
-				//i.ax = 0; i.ay = 0;
-			}
-		}
-	});
-	 */
-
-	/*
-	 * Velocity
-	 */
-	i.x += i.vx; //add velocity
-	i.y += i.vy;
-	
-	/* 
-	 * Remove once off screen
-	 */
-	if (i.y - i.r > c.height) objects.shift();
-}
-
-function draw() {
-	ctx.clearRect(0, 0, c.width, c.height); //clear screen
-	objects.forEach(function(i) {
-		ctx.beginPath();
-		ctx.fillStyle = '#e0e0e0'; //transparency
-		ctx.arc(i.x, i.y, i.r, 0, 2 * Math.PI);//draw ball
-		ctx.fill();
-	});
-}
-
-function spawnBall() {
-	objects.push(
-		new Ball(
-			Math.random() * c.width, -8, //pos
-			Math.random() - 0.5, 0, //vel
-			Math.random() * (maxSize - minSize) + minSize //size
-		)
-	);
+	//ANIMATION SPECIFIC
+	togglePaused();
+	ctx.lineWidth = 0.1;
+	rect.offsetX = c.width / 2;
+	rect.offsetY = c.height / 2;
+	//ANIMATION SPECIFIC
 }
 
 function togglePaused() {
@@ -111,3 +32,55 @@ function togglePaused() {
 
 //start called when page is loaded
 window.onload = start;
+
+//ANIMATION SPECIFIC
+var rect = {
+	't': 0,
+	'cwidth': 100, 'cheight': 100,
+	'offsetX': 530, 'offsetY': 400,
+	'a': Math.random() * Math.PI*2, 'b': Math.random() * Math.PI*2,
+	'update': function() {
+		this.t += 0.01;
+		this.x1 =	Math.sin(this.t + this.a) * this.cwidth;
+		this.y1 =	Math.sin(this.t + this.b) * this.cheight;
+		this.width =	Math.sin(this.t + this.a) * this.cwidth;
+		this.height =	Math.sin(this.t + this.b) * this.cheight;
+		if (this.t > Math.PI*2) {
+			paused = true;
+			this.t = 0;
+			this.a = Math.random() * Math.PI*2;
+			this.b = Math.random() * Math.PI*2;
+			window.setTimeout(function() {
+				paused = false;
+				ctx.clearRect(0, 0, c.width, c.height); //clear screen
+			}, 1500);
+		}
+	},
+	'draw': function() {
+		ctx.strokeRect(
+			this.x1 + this.offsetX,
+			this.y1 + this.offsetY,
+			this.width,
+			this.height
+		);
+	}
+}
+//ANIMATION SPECIFIC
+
+function update() {
+	if (!paused) {
+		// ANIMATION SPECIFIC //
+		rect.update();
+		draw();
+		// ANIMATION SPECIFIC //
+	}
+	window.requestAnimationFrame(update);
+}
+
+
+function draw() {
+	//ctx.clearRect(0, 0, c.width, c.height); //clear screen
+	// ANIMATION SPECIFIC //
+	rect.draw(); 
+	// ANIMATION SPECIFIC //
+}
