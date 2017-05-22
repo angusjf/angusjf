fbe4e53334090e264e5bde337bcff034ceb4c8cd
Angus Macbook <angus.findlay.mb@gmail.com>
2017-05-22 17:58:51 +0100

    changed animation to cool grid

--- script.js
+++ script.js
@@ -1,82 +1,10 @@
-var canvasName = "magic-canvas";
-var ctx, c;
-var paused = true;
-var closeButton;
-
-function start() {
-	closeButton = document.createElement("button");
-	closeButton.classList.add('link');
-	closeButton.id = 'close-button';
-	closeButton.onclick = togglePaused;
-	closeButton.innerHTML = '&#9654;';
-	document.getElementById("container").appendChild(closeButton);
-	c = document.getElementById(canvasName);
-	c.width = document.body.clientWidth;
-	c.height = document.body.clientHeight;
-	c.onselectstart = function () { return false; }
-	ctx = c.getContext("2d");
-	ctx.font = "20px Menlo";
-	window.requestAnimationFrame(update);
-	//ANIMATION SPECIFIC
-	togglePaused();
-	ctx.lineWidth = 0.1;
-	circ.offsetX = c.width / 2;
-	circ.offsetY = c.height / 1.75;
-	//ANIMATION SPECIFIC
-}
-
-function togglePaused() {
-	paused = !paused;
-	if (paused) ctx.clearRect(0, 0, c.width, c.height); //clear screen
-	closeButton.innerHTML = paused ? "&#9654;" : "&times";
-}
-
-//start called when page is loaded
-window.onload = start;
-
-var initialT = -6;
-var speed = 0.05;
-var maxT = Math.PI * 3;
-var delay = 500;
-
-var circ = {
-	't': initialT,
-	'xScale': 100, 'yScale': 100, 'rScale': 1,
-	'offsetX': 530, 'offsetY': 400,
-	'a': 1 * Math.random() * Math.PI*2,
-	'b': 1 * Math.random() * Math.PI*2,
-	'c': 1 * Math.random() * Math.PI*2,
-	'update': function() {
-		if (this.t > maxT) {
-			paused = true;
-			this.t = initialT;
-			this.a = 1 * Math.random() * Math.PI*2;
-			this.b = 1 * Math.random() * Math.PI*2;
-			window.setTimeout(function() {
-				paused = false;
-			}, delay);
+function toggleBoxes() {
+	var boxes = document.getElementsByClassName("box");
+	Array.prototype.forEach.call(boxes, function (box) {
+		if (box.style.display === "inline-block") {
+			box.style.display = "none";
+		} else {
+			box.style.display = "inline-block";
 		}
-		this.x1 =		Math.sin(this.t + this.a) * this.xScale;
-		this.y1 =		Math.sin(this.t + this.b) * this.yScale;
-		this.radius =  Math.abs(Math.exp(this.t + this.c) * this.rScale);
-		this.t += speed;
-	},
-	'draw': function() {
-		ctx.beginPath();
-		ctx.arc(this.x1 + this.offsetX, this.y1 + this.offsetY, this.radius, 0, 2 * Math.PI);
-		ctx.stroke();
-	}
-}
-
-function update() {
-	if (!paused) {
-		circ.update();
-		draw();
-	}
-	window.requestAnimationFrame(update);
-}
-
-
-function draw() {
-	circ.draw(); 
+	});
 }
