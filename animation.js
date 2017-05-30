56753fb956f6ab113311538ec1560a8d7243d45b
Angus Macbook <angus.findlay.mb@gmail.com>
2017-05-30 08:46:32 +0100

    better on mobile

--- animation.js
+++ animation.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var canvasName = "magic-canvas";
+var canvasName = "animation-canvas";
 var ctx, c;
 var mouse = { x: 0, y: 0 };
 
 function setup() {
 	//canvas
 	c = document.getElementById(canvasName);
-	c.width = document.body.clientWidth;
-	c.height = document.body.clientHeight;
+	c.width = 170;
+	c.height = 170;
 	c.onselectstart = function () { return false; }
 	ctx = c.getContext("2d");
 	//loops
@@ -17,25 +17,29 @@ function setup() {
 }
 
 document.onmousemove = function (e) {
-	mouse.x = e.clientX;
-	mouse.y = e.clientY;
+	mouse.x = e.clientX - c.getBoundingClientRect().left;
+	mouse.y = e.clientY - c.getBoundingClientRect().top;
 }
 
 var points = [];
-var gridSize = 32;
+var gridSize = 16;
 var offset = {'x': 0, 'y': 0};
-var separation = {'x': 7, 'y': 10};
+var separation = {'x': 8, 'y': 8};
 
 function start() {
-	ctx.lineWidth = 2;
+	ctx.lineWidth = 1;
 	ctx.strokeStyle = "#757575";
-	//separation.x = c.width / (gridSize - 1);
-	//separation.y = c.height / (gridSize - 1);
 	offset.x = c.width/2;
-	offset.y = c.height/1.7;
+	offset.y = c.height/2;
 	for (var i = 0; i < gridSize; i ++) {
 		for (var j = 0; j < gridSize; j ++) {
-			points[i*gridSize + j] = {'gridX': (j - gridSize / 2) * separation.x + offset.x, 'gridY': (i - gridSize / 2) * separation.y + offset.y, 'rand': 2*Math.random()};
+			points[i*gridSize + j] = {
+				'gridX': (j - gridSize / 2) * separation.x + offset.x,
+				'gridY': (i - gridSize / 2) * separation.y + offset.y,
+				'x': 0,
+				'y': 0,
+				'rand': Math.random()
+			};
 		}
 	}
 }
@@ -46,14 +50,27 @@ function update() {
 
 	for (var i = 0; i < gridSize; i ++) {
 		for (var j = 0; j < gridSize; j ++) {
-			points[i*gridSize + j].x = points[i*gridSize + j].gridX;
-			points[i*gridSize + j].y = points[i*gridSize + j].gridY
-				- 20 * Math.min(2,Math.pow(Math.abs((mouse.x - points[i*gridSize + j].gridX) * 0.04), -2))
-				* (0.7 + 0.3*Math.pow(Math.sin(points[i*gridSize + j].rand + time), 2));
+			points[i*gridSize + j].x = lerp(
+				points[i*gridSize + j].x,
+				points[i*gridSize + j].gridX + (mouse.x - c.width / 2) * (0.3 * points[i*gridSize + j].rand + 0.7),
+				0.1
+			);
+			points[i*gridSize + j].y = lerp(
+				points[i*gridSize + j].y,
+				points[i*gridSize + j].gridY + (mouse.y - c.height / 2) * (0.3 * points[i*gridSize + j].rand + 0.7),
+				0.1
+			);
 
-			if (j > 0) {
+			if (j > 0 && points[i*gridSize + j]) {
 				ctx.beginPath();
 				ctx.moveTo(points[i*gridSize + j - 1].x, points[i*gridSize + j - 1].y);
+				ctx.lineTo(points[i*gridSize + j].x, points[i*gridSize + j].y);
+				ctx.stroke();
+			}
+
+			if (i > 0 && points[i*gridSize + j]) {
+				ctx.beginPath();
+				ctx.moveTo(points[(i - 1)*gridSize + j].x, points[(i - 1)*gridSize + j].y);
 				ctx.lineTo(points[i*gridSize + j].x, points[i*gridSize + j].y);
 				ctx.stroke();
 			}
@@ -63,4 +80,8 @@ function update() {
 	time += dt;
 
 	window.requestAnimationFrame(update);
+}
+
+function lerp(v0, v1, t) {
+	return v0*(1-t)+v1*t;
 }
--- script.js
+++ script.js
@@ -5,11 +5,22 @@ window.onload = function () {
 	Array.prototype.forEach.call(boxes, function (box) {
 		var closeButton = document.createElement("button");
 		closeButton.classList.add('close-button');
-		closeButton.onclick = function () {
-			box.style.display = "none";
-		};
-		closeButton.innerHTML = '&times;';
 		box.appendChild(closeButton);
+
+		box.originalHeight = (box.offsetHeight - 40) + "px";
+		box.style.height = box.originalHeight;
+		closeButton.innerHTML = '&times;';
+
+		closeButton.onclick = function () {
+			box.closed = !box.closed;
+			if (box.closed) {
+				box.style.height = "16px";
+				closeButton.innerHTML = '+';
+			} else {
+				box.style.height = box.originalHeight;
+				closeButton.innerHTML = '&times;';
+			}
+		};
 	});
 	setup();
 }
