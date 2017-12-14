3f262a27523e4dc12e33dcd000fabb7bd7f50c18
Angus Macbook <angus.findlay.mb@gmail.com>
2017-12-14 16:14:17 +0000

    black and white redesign

--- animation.js
+++ animation.js
@@ -1,91 +1,27 @@
-'use strict';
-
-var canvasName = "animation-canvas";
-var ctx, c;
-var mouse = {onCanvas: false, x: 0, y: 0 };
-
-function setup() {
-	//canvas
-	c = document.getElementById(canvasName);
-	c.width = 170;
-	c.height = 170;
-	c.onselectstart = function () { return false; }
+var c, ctx;
+window.onload = function () {
+	c = document.getElementById('canvas');
 	ctx = c.getContext("2d");
-	//loops
-	start();
-	window.requestAnimationFrame(update);
-	c.onmouseleave = function (e) {
-		mouse.onCanvas = false;
-	};
-	c.onmouseenter = function (e) {
-		mouse.onCanvas = true;
-	};
+	c.onselectstart = function () { return false; }
+	c.width = window.outerWidth;
+	c.height = window.outerHeight;
+	ctx.fillStyle = "#fbfbfb";
+	ctx.font = "20px Helvetica Neue"
+	draw();
 }
-
-document.onmousemove = function (e) {
-	mouse.x = e.clientX - c.getBoundingClientRect().left;
-	mouse.y = e.clientY - c.getBoundingClientRect().top;
-}
-
-var points = [];
-var gridSize = 16;
-var offset = {'x': 0, 'y': 0};
-var separation = {'x': 16, 'y': 16};
-
-function start() {
-	ctx.lineWidth = 1;
-	ctx.strokeStyle = "#757575";
-	offset.x = c.width/2;
-	offset.y = c.height/2;
-	for (var i = 0; i < gridSize; i ++) {
-		for (var j = 0; j < gridSize; j ++) {
-			points[i*gridSize + j] = {
-				'gridX': (j - gridSize / 2) * separation.x + offset.x,
-				'gridY': (i - gridSize / 2) * separation.y + offset.y,
-				'x': 0,
-				'y': 0,
-				'rand': Math.random()
-			};
-		}
+var x = 0, y = 0, w = 10, h = 10, str = 'a';
+var i = 0, max1 = 20, max2 = 80;
+function draw() {
+	requestAnimationFrame(draw);
+	i++;
+	if (i % max2 == 0)
+	ctx.clearRect(0, 0, c.width, c. height);
+	if (i % max1 == 0) {
+		x = Math.random() * c.width;
+		y = Math.random() * c.height;
+		w = Math.random() * c.width;
+		h = Math.random() * c.height;
+		str = String.fromCharCode(32 + Math.random() * 95);
+		ctx.fillText(str, x, y);
 	}
-}
-
-var time = 0, dt = 0.02;
-function update() {
-	ctx.clearRect(0, 0, c.width, c.height);
-
-	ctx.beginPath();
-	for (var i = 0; i < gridSize; i ++) {
-		for (var j = 0; j < gridSize; j ++) {
-			points[i*gridSize + j].x = lerp(
-				points[i*gridSize + j].x,
-				points[i*gridSize + j].gridX + ((mouse.onCanvas ? (mouse.x - c.width / 2) : Math.cos(time) * c.width / 10)) * (0.3 * points[i*gridSize + j].rand + 0.7),
-				0.1
-			);
-			points[i*gridSize + j].y = lerp(
-				points[i*gridSize + j].y,
-				points[i*gridSize + j].gridY + ((mouse.onCanvas ? (mouse.y - c.height / 2) : Math.sin(time) * c.height / 10)) * (0.3 * points[i*gridSize + j].rand + 0.7),
-				0.1
-			);
-
-			if (j > 0 && points[i*gridSize + j]) {
-				ctx.moveTo(points[i*gridSize + j - 1].x, points[i*gridSize + j - 1].y);
-				ctx.lineTo(points[i*gridSize + j].x, points[i*gridSize + j].y);
-			}
-
-			if (i > 0 && points[i*gridSize + j]) {
-				ctx.moveTo(points[(i - 1)*gridSize + j].x, points[(i - 1)*gridSize + j].y);
-				ctx.lineTo(points[i*gridSize + j].x, points[i*gridSize + j].y);
-			}
-		}
-	}
-	ctx.stroke();
-
-	time += dt;
-
-	window.requestAnimationFrame(update);
-}
-
-function lerp(v0, v1, t) {
-	return v0*(1-t)+v1*t;
 }
