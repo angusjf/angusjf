769acadb825cfcd5dccecc0113b17cbe9d96109b
Angus Macbook <angus.findlay.mb@gmail.com>
2017-12-29 15:19:45 +0000

    font backup, link border

--- animation.js
+++ animation.js
@@ -10,25 +10,23 @@ window.onload = function () {
 	ctx.font = "20px Helvetica Neue"
 	draw();
 }
-var x = 0, y = 0, w = 10, h = 10, str = 'a';
-var t = 0, drawTime = 20, clearTime = 600, glitchTime = 75, resetGlitchTime = glitchTime + 50;
+var x = 0, y = 0;
+var drawTime = 20, clearTime = 600, glitchTime = 75, resetGlitchTime = glitchTime + 50;
+var t = clearTime;
 function draw() {
 	requestAnimationFrame(draw);
-	t++;
 	if (t % clearTime == 0) {
 		ctx.clearRect(0, 0, c.width, c. height);
 	}
 	if (t % drawTime == 0) {
 		x = Math.random() * c.width;
 		y = Math.random() * c.height;
-		w = Math.random() * c.width;
-		h = Math.random() * c.height;
 		str = String.fromCharCode(32 + Math.random() * 95);
 		ctx.fillText(str, x, y);
 	}
 	if (t % glitchTime == 0) {
-		ctx.font = "20px Wingdings";
-		divs[Math.floor(Math.random() * (divs.length - 1))].style.fontFamily = "wingdings";
+		ctx.font = "20px Wingdings, Times";
+		divs[Math.floor(Math.random() * (divs.length - 1))].style.fontFamily = "wingdings, times";
 	}
 	if (t % resetGlitchTime == 0) {
 		ctx.font = "20px Helvetica Neue";
@@ -36,4 +34,5 @@ function draw() {
 			divs[i].style.fontFamily = "Helvetica Neue";
 		}
 	}
+	t++;
 }
