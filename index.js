5c1d1574feaa31c1bf9416d9820a66fd0be4d32b
Angus Findlay <angusf@theodo.co.uk>
2022-04-03 10:04:06 +0100

    mobile styling fixes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import "https://use.fontawesome.com/releases/v5.0.13/js/all.js"
 export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
-    console.log("App loaded", app);
+    // console.log("App loaded", app);
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";
