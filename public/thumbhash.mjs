c27f24e4c62ca1fc7d2f47d56c427ff25c467fb6
Angus Findlay <angus.findlay@icloud.com>
2023-04-11 13:39:16 +0100

    img[data-thumbhash]

--- public/thumbhash.mjs
+++ public/thumbhash.mjs
@@ -1,6 +1,6 @@
 import { thumbHashToDataURL } from "https://cdn.skypack.dev/thumbhash"
 
-document.querySelectorAll("main.homepage>article.card>img").forEach(img => {
+document.querySelectorAll("img[data-thumbhash]").forEach(img => {
     const thumbhash = Uint8Array.from(atob(img.dataset.thumbhash), c => c.charCodeAt(0))
     img.style.background = `center / cover url(${thumbHashToDataURL(thumbhash)})`
 })
