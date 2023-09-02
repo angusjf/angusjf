370875b16b6c355f90e3e87f17696a86114789bf
Angus Findlay <angus.findlay@prima.it>
2023-09-02 19:26:23 +0100

    performance refactor

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ extern crate tinytemplate;
 
 #[path = "js.rs"]
 mod js;
-use base64::{engine::general_purpose, Engine as _};
+use base64::{engine::general_purpose, Engine};
 use chrono::NaiveDate;
 use lol_html::{element, HtmlRewriter, Settings};
-use minify_js::{minify, Session, TopLevelMode};
 use serde::{de, Deserializer, Serializer};
 use serde_yaml;
 use std::{
+    collections::HashMap,
     env, fmt,
     fs::{self, read_to_string, File},
-    io::{self, Write},
+    io::{self},
     path::{Path, PathBuf},
+    sync::{Arc, Mutex},
+    thread::spawn,
     time::Instant,
 };
 use thumbhash::rgba_to_thumb_hash;
@@ -24,6 +26,8 @@ const TITLE: &str = "Angus Findlay";
 const DESCRIPTION: &str = "Angus Findlay's Blog - angusjf";
 const CANONICAL_ORIGIN: &str = "https://angusjf.com";
 const IMG: &str = "/images/plants.webp";
+const ROOT_IMG_RENDERED_SIZE: u32 = 244;
+const SRCSET_RESOLUTIONS: [u32; 2] = [1, 2];
 
 #[derive(Deserialize)]
 struct ExperimentMetadata {
@@ -214,22 +218,7 @@ fn index(cards: Vec<Card>) -> Root {
     }
 }
 
-fn get_encoded_thumbhash(img_url: &str) -> Box<str> {
-    let image = image::open(img_url).unwrap();
-    print!("{img_url}");
-    io::stdout().flush().unwrap();
-
-    let small = image.thumbnail(100, 100);
-    let thumb_hash = rgba_to_thumb_hash(
-        small.width() as usize,
-        small.height() as usize,
-        &small.to_rgba8().into_raw(),
-    );
-    println!(" (hashed)");
-    general_purpose::STANDARD.encode(thumb_hash).into()
-}
-
-fn optimize_assets(html: &str) -> Box<str> {
+fn optimize_assets(html: &str, thumbhashes: HashMap<Box<str>, (String, u32)>) -> Box<str> {
     let mut output = vec![];
 
     let mut rewriter = HtmlRewriter::new(
@@ -256,12 +245,6 @@ fn optimize_assets(html: &str) -> Box<str> {
 
                     let code = crate::js::bundle(path);
 
-                    // let session = Session::new();
-                    // let mut out = Vec::new();
-                    // minify(&session, TopLevelMode::Global, &code.into_bytes(), &mut out).unwrap();
-
-                    // let compressed: String = String::from_utf8(out).unwrap();
-
                     let content = format!("<script type='module'>{code}</script>");
 
                     el.replace(&content, lol_html::html_content::ContentType::Html);
@@ -269,68 +252,52 @@ fn optimize_assets(html: &str) -> Box<str> {
                     Ok(())
                 }),
                 element!("img[src^='/']", |el| {
-                    let rendered_size = 244;
-
                     let src = el.get_attribute("src").unwrap();
                     let src = src.as_str();
-                    print!("{}", src);
-                    io::stdout().flush().unwrap();
 
-                    let path = "public".to_owned() + src;
-                    let image = image::open(&path).unwrap();
+                    let image_width = thumbhashes[src].1;
 
-                    let make = |pixel_ratio: u32| {
-                        let resized = image.resize(
-                            rendered_size * pixel_ratio,
-                            rendered_size * pixel_ratio,
-                            image::imageops::FilterType::Lanczos3,
-                        );
-                        let in_path = Path::new(&path);
-                        let ext = in_path.extension().unwrap().to_str().unwrap();
-                        let out_path = in_path.with_file_name(format!(
-                            "{}_{pixel_ratio}x.{}",
-                            in_path
-                                .file_name()
-                                .unwrap()
-                                .to_str()
-                                .unwrap()
-                                .trim_end_matches(ext)
-                                .trim_end_matches("."),
-                            ext
-                        ));
+                    let true_scale: f32 = image_width as f32 / ROOT_IMG_RENDERED_SIZE as f32;
 
-                        resized.save(&out_path).unwrap();
+                    fn filesize(path: &Path) -> u64 {
+                        File::open(path).unwrap().metadata().unwrap().len()
+                    }
 
-                        let src_scaled;
+                    let in_path = "public".to_owned() + src;
+                    let in_path = Path::new(&in_path);
+                    let ext = in_path.extension().unwrap().to_str().unwrap();
+                    let original_size = filesize(in_path);
 
-                        fn filesize(path: &Path) -> u64 {
-                            File::open(path).unwrap().metadata().unwrap().len()
-                        }
+                    let mut srcset = SRCSET_RESOLUTIONS
+                        .map(|pixel_ratio| {
+                            let out_path = in_path.with_file_name(format!(
+                                "{}_{pixel_ratio}x.{}",
+                                in_path
+                                    .file_name()
+                                    .unwrap()
+                                    .to_str()
+                                    .unwrap()
+                                    .trim_end_matches(ext)
+                                    .trim_end_matches("."),
+                                ext
+                            ));
 
-                        let original_size = filesize(in_path);
-                        let new_size = filesize(&out_path);
+                            let new_size = filesize(&out_path);
 
-                        print!(
-                            " [@{pixel_ratio}x compression = {}%]",
-                            ((original_size as f32 - new_size as f32) / (original_size as f32))
-                                * 100.0
-                        );
-
-                        if new_size > original_size {
-                            // don't use the downsized image, if downsizing didn't make it smaller
-                            src_scaled = src;
-                        } else {
-                            src_scaled = out_path.to_str().unwrap().strip_prefix("public").unwrap();
-                        }
-
-                        return format!("{src_scaled} {pixel_ratio}x");
-                    };
-
-                    let true_scale: f32 = image.width() as f32 / rendered_size as f32;
-                    let srcset = [make(1), make(2), format!("{src} {true_scale}x")].join(", ");
+                            if new_size < original_size {
+                                // compression achieved
+                                let src_scaled =
+                                    out_path.to_str().unwrap().strip_prefix("public").unwrap();
+                                format!("{src_scaled} {pixel_ratio}x")
+                            } else {
+                                format!("{src} {pixel_ratio}x")
+                            }
+                        })
+                        .join(", ");
+                    srcset.push_str(", ");
+                    srcset.push_str(&format!("{src} {true_scale}x"));
 
                     el.set_attribute("srcset", &srcset).unwrap();
-                    println!(".");
 
                     Ok(())
                 }),
@@ -346,22 +313,92 @@ fn optimize_assets(html: &str) -> Box<str> {
     String::from_utf8(output).unwrap().into_boxed_str()
 }
 
+fn process_images(cards: &[Card]) -> HashMap<Box<str>, (String, u32)> {
+    let filter = if env::args().nth(1).is_some_and(|flag| flag == "--dev") {
+        image::imageops::FilterType::Nearest
+    } else {
+        image::imageops::FilterType::Lanczos3
+    };
+
+    let thumbhashes = Arc::new(Mutex::new(HashMap::new()));
+
+    let mut handles = vec![];
+
+    for card in cards {
+        let src = card.img_url.clone();
+        let thumbhashes = thumbhashes.clone();
+        handles.push(spawn(move || {
+            let path = Arc::new("public".to_owned() + src.as_ref());
+            let image = Arc::new(image::open(&*path).unwrap());
+
+            let resize_handles = [1, 2].map(|pixel_ratio| {
+                let image = image.clone();
+                let path = path.clone();
+                spawn(move || {
+                    let in_path = Path::new(&*path);
+                    let ext = in_path.extension().unwrap().to_str().unwrap();
+                    let out_path = in_path.with_file_name(format!(
+                        "{}_{pixel_ratio}x.{}",
+                        in_path
+                            .file_name()
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                            .trim_end_matches(ext)
+                            .trim_end_matches("."),
+                        ext
+                    ));
+                    let resized = image.resize(
+                        ROOT_IMG_RENDERED_SIZE * pixel_ratio,
+                        ROOT_IMG_RENDERED_SIZE * pixel_ratio,
+                        filter,
+                    );
+
+                    resized.save(&out_path).unwrap();
+                })
+            });
+            let hash_handle = spawn(move || {
+                let thumb = image.thumbnail(100, 100);
+                let thumb_hash = rgba_to_thumb_hash(
+                    thumb.width() as usize,
+                    thumb.height() as usize,
+                    &thumb.to_rgba8().into_raw(),
+                );
+
+                let thumbhash = general_purpose::STANDARD.encode(thumb_hash);
+
+                thumbhashes
+                    .lock()
+                    .unwrap()
+                    .insert(src, (thumbhash, image.width()));
+            });
+            hash_handle.join().unwrap();
+            for handle in resize_handles {
+                handle.join().unwrap();
+            }
+        }))
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let thumbhashes = Arc::try_unwrap(thumbhashes).unwrap();
+
+    thumbhashes.into_inner().unwrap()
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let start = Instant::now();
-
+    let start_ = Instant::now();
     let root_template = fs::read_to_string("templates/root.html")?;
+
     let mut tt = TinyTemplate::new();
     tt.add_template("root", &root_template).unwrap();
     tt.add_formatter("markdown", |md, str| {
         let md = md.as_str().unwrap();
         let parser = pulldown_cmark::Parser::new(&md);
         pulldown_cmark::html::push_html(str, parser);
-        Ok(())
-    });
-    tt.add_formatter("thumbhash", |img_url, str| {
-        let img_url = "public".to_string() + img_url.as_str().unwrap();
-        str.push_str(&get_encoded_thumbhash(&img_url));
         Ok(())
     });
 
@@ -399,6 +436,8 @@ async fn main() -> std::io::Result<()> {
                 .map(blogpost_to_card),
         )
         .collect();
+    println!("built blog posts in {}s", start_.elapsed().as_secs_f32());
+    let start = Instant::now();
 
     cards.sort_by(|a, b| b.date.cmp(&a.date));
 
@@ -430,20 +469,30 @@ async fn main() -> std::io::Result<()> {
             ],
         },
     );
+    let thumbhashes = process_images(&cards);
+    println!("optimized images in {}s", start.elapsed().as_secs_f32());
+    let start = Instant::now();
+
+    {
+        let thumbhashes = thumbhashes.clone();
+        tt.add_formatter("thumbhash", move |img_url, str| {
+            str.push_str(&thumbhashes[img_url.as_str().unwrap()].0);
+            Ok(())
+        });
+    }
 
     let index = tt.render("root", &index(cards)).unwrap();
 
-    let index = if env::args().nth(1).is_some_and(|flag| flag == "--dev") {
-        index.into()
-    } else {
-        optimize_assets(&index)
-    };
+    println!("built root page in {}s", start.elapsed().as_secs_f32());
+    let start = Instant::now();
+    let index = optimize_assets(&index, thumbhashes);
+    println!("optimized assets in {}s", start.elapsed().as_secs_f32());
 
     fs::write("dist/index.html", &*index).unwrap();
 
     println!(
         "{CANONICAL_ORIGIN} built in {}s",
-        start.elapsed().as_secs_f32()
+        start_.elapsed().as_secs_f32()
     );
     Ok(())
 }
